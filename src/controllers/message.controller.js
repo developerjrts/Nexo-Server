0bfe099a09b9d6d28b8fc17f7dbd63d49468f95f
developerjrts <developer.jrts@gmail.com>
2025-10-23 19:23:01 +0530

    push

---
+++ src/controllers/message.controller.js
@@ -0,0 +1,30 @@
+import messageModel from "../models/message.model.js";
+
+export const getMessages = async (req, res) => {
+  try {
+    const receiverId = req.params.receiverId;
+    const userId = req.user._id;
+
+    const messages = await messageModel
+      .find({
+        $or: [
+          { senderId: userId, receiverId: receiverId },
+          { senderId: receiverId, receiverId: userId },
+        ],
+      })
+      .populate("senderId", "name username avatar")
+      .populate("receiverId", "name username avatar");
+
+    res.status(200).json({
+      status: true,
+      message: "messages sent",
+      messages,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(501).json({
+      message: error.message,
+      status: false,
+    });
+  }
+};
--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -1,5 +1,11 @@
 import express from "express";
+import verifyUser from "../middlewares/auth.middleware.js";
+import * as messageController from "../controllers/message.controller.js";
 
 const messageRoutes = express.Router();
+
+messageRoutes
+  .route("/messages/:receiverId")
+  .get(verifyUser, messageController.getMessages);
 
 export default messageRoutes;
